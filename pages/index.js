cd5b5239e1e7525d264c1a076156d91b142ca715
Nicolas Maluleke <nicolusmaluleke@gmail.com>
2022-01-28 01:18:35 +0200

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -8,34 +8,33 @@ import Floor from "../components/Floor";
 import Bulb from "../components/Bulb";
 import ColorPicker from "../components/ColorPicker";
 import Draggable from "../components/Draggable";
-import {Suspense} from "react";
+import { Suspense } from "react";
+import { Physics } from '@react-three/cannon';
+import Model from '../components/Model';
+import BoundingBox from '../components/BoundingBox';
+import Cars from '../components/Cars';
 
 export default function Home() {
   return (
     <div className={styles.scene}>
-          <ColorPicker/>
+      <ColorPicker />
       <Canvas className={styles.canvas}
         shadows={true}
         camera={{
           position: [7, 7, 7]
         }}
       >
+        <fog args={['white', 1, 10]} />
         <ambientLight intensity={0.2} />
-          <Draggable>
-              <Bulb position={[-3, 2, 0]} />
-              <Bulb position={[0, 3, 0]} />
-              <Suspense fallback={null}>
-                  <Box position={[-4, 1, 0]} />
-              </Suspense>
-              <Suspense fallback={null}>
-                  <Box position={[4, 1, 0]} />
-              </Suspense>
-              <Suspense fallback={null}>
-                  <Background />
-              </Suspense>
-          </Draggable>
-        <Floor />
-        <Orbit  />
+        <Bulb position={[0, 3, 0]} />
+        <Physics>
+          <Cars />
+          <Suspense fallback={null}>
+            <Background />
+          </Suspense>
+          <Floor />
+        </Physics>
+        <Orbit />
         <axesHelper args={[5]} />
       </Canvas>
     </div>
