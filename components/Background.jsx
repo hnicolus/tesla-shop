a57fd52cf9da3d54fe927d92ff897a3ba56e31f0
Nicolas Maluleke <nicolusmaluleke@gmail.com>
2022-01-28 01:16:39 +0200

    Added background

--- components/Background.jsx
+++ components/Background.jsx
@@ -1,18 +1,22 @@
-import {useLoader, useThree} from "@react-three/fiber";
+import { useLoader, useThree } from "@react-three/fiber";
 import * as THREE from "three";
-import {Suspense} from "react";
+import { Suspense, useMemo } from "react";
 
 const Background = () => {
     const texture = useLoader(THREE.TextureLoader,
-        "autoshop.jpg");
-    const {gl} = useThree();
-    ///const formatted = new THREE.WebGLCubeRenderTarget(texture.image.height).fromEquirectangularTexture(gl,texture);
+        "sky.jpg");
+    const { gl } = useThree();
+
+    const formatted = useMemo(() => {
+        return new THREE.WebGLCubeRenderTarget(texture.image.height)
+            .fromEquirectangularTexture(gl, texture);
+    }, [])
 
     return (
         <Suspense fallback={null}>
-        <primitive
-            attach="background"
-            object={texture} />
+            <primitive
+                attach="background"
+                object={texture} />
         </Suspense>
     )
 }
